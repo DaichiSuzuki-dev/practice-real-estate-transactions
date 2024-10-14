6f250d6414497996594d1a0d3dbc6ea2523ceb5d
DESKTOP-B8HCDBO\Amera <merasutoma@gmail.com>
2024-10-15 03:43:52 +0900

    FontAwesome の追加。及び UI コンポ―ネント fa-window-restore の追加。

---
+++ react-project/src/components/ui/page-link-icon.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWindowRestore } from "@fortawesome/free-regular-svg-icons";
+
+
+const PageLinkIcon: React.FC = () => {
+  return (
+    <FontAwesomeIcon icon={faWindowRestore}/>
+  );
+};
+
+export default PageLinkIcon;
