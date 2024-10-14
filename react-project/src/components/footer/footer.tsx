601bd3d144ca4a53be0175a4d4c338bfa97b6fc5
DESKTOP-B8HCDBO\Amera <merasutoma@gmail.com>
2024-10-15 04:42:01 +0900

    add Footer

---
+++ react-project/src/components/footer/footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.scss';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className='footer'>
+      <p className='footer-copyright'>© 2023 Landit Inc.</p>
+    </footer>
+  );
+};
+
+export default Footer
