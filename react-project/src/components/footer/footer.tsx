2949e06d4fa03eba2dd490ed916e0297494c1ec0
DESKTOP-B8HCDBO\Amera <merasutoma@gmail.com>
2024-10-15 13:41:30 +0900

    Footer 完成

--- react-project/src/components/footer/footer.tsx
+++ react-project/src/components/footer/footer.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import './footer.scss';
+import React from "react";
+import "./footer.scss";
+import FooterNav from "../footer-nav/footer-nav";
 
 const Footer: React.FC = () => {
   return (
-    <footer className='footer'>
-      <p className='footer-copyright'>© 2023 Landit Inc.</p>
+    <footer className="footer">
+      <div className="footer-navigation footerContent">
+        <FooterNav pageName="利用規約" />
+        <FooterNav pageName="プライバシーポリシー" />
+      </div>
+
+      <p className="footer-copyright footerContent">© 2023 Landit Inc.</p>
     </footer>
   );
 };
 
-export default Footer
+export default Footer;
--- react-project/src/components/ui/page-link-icon.tsx
+++ react-project/src/components/ui/page-link-icon.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWindowRestore } from "@fortawesome/free-regular-svg-icons";
 
+interface PageLinkIconProps {
+  className?: string;
+}
 
-const PageLinkIcon: React.FC = () => {
-  return (
-    <FontAwesomeIcon icon={faWindowRestore}/>
-  );
+const PageLinkIcon: React.FC<PageLinkIconProps> = ({ className }) => {
+  return <FontAwesomeIcon icon={faWindowRestore} className={className} />;
 };
 
 export default PageLinkIcon;
