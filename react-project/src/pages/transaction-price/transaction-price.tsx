e37ef2e5965a883d5e74ed687efcafb7e606d672
DESKTOP-B8HCDBO\Amera <merasutoma@gmail.com>
2024-10-17 02:28:53 +0900

    mainエリアのheader完成

--- react-project/src/pages/transaction-price/transaction-price.tsx
+++ react-project/src/pages/transaction-price/transaction-price.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import "./transaction-price.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquarePollVertical } from "@fortawesome/free-solid-svg-icons";
 
 const TransactionPrice: React.FC = () => {
   return (
     <div className="main-transactionPrice" style={{ backgroundImage: `url('/images/map-image.png')` }}>
-      <div className="main-transactionPrice-background"></div>
+      <div className="main-transactionPrice-background">
+        <section className="titleContent">
+          <div className="titleContent-container">
+            <div className="titleContent-container-title">
+              <FontAwesomeIcon icon={faSquarePollVertical} className="titleContent-container-title-icon" />
+              <h1 className="titleContent-container-title-text">取引価格</h1>
+            </div>
+            <p className="titleContent-container-description">※取引価格1㎡あたり</p>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
